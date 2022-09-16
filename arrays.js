9d906719c5341f78fac20ad422df105d4cf496c4
Peyton Moss <pmoss94@gmail.com>
2022-09-16 08:30:25 -0600

    arrays finished

--- arrays.js
+++ arrays.js
@@ -4,11 +4,15 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+const colorCopy = faveColors.slice(0, faveColors.length);
+console.log(colorCopy);
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('mauve');
+console.log(colorCopy);
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -19,6 +23,8 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
+const middleNums = numbers.slice(1, numbers.length - 1);
+console.log(middleNums);
 
 
 //////////////////PROBLEM 4////////////////////
@@ -34,3 +40,12 @@ const numbers = [1, 2, 3, 4, 5]
 const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
 
 // CODE HERE
+const answers = [];
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answers.push('big');
+  } else {
+    answers.push('small');
+  }
+}
+console.log(answers);
